8b5af1f9233dfe954be1b55b8cc0e2ba90f491bd
Huima Jose <imoicalda@gmail.com>
2025-01-13 16:26:14 +0100

    INstalled radix theme package

---
+++ src/app/components/parts/LogoHeader.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+type LogoHeaderProps = {
+  altText?: string; // Texto alternativo para acessibilidade
+  href?: string; // Link para onde o logo redireciona, caso aplicável
+};
+
+const LogoHeader: React.FC<LogoHeaderProps> = ({
+  altText = "Logo",
+  href = "/",
+}) => {
+  return (
+    <header className="flex p-4 bg-white ml-10">
+      <Link href={href} passHref>
+        
+          <Image src="/logo-kubico-facil.jpg" alt={altText} width={80} height={80} />
+       
+      </Link>
+    </header>
+  );
+};
+
+export default LogoHeader;
