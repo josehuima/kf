e4a4a45f702b324525ad792ee14fbea8d7bca608
Huima Jose <imoicalda@gmail.com>
2025-02-01 11:50:39 +0100

    Removed Real-stae schema from context file

--- src/app/components/parts/LogoHeader.tsx
+++ src/app/components/parts/LogoHeader.tsx
@@ -26,6 +26,9 @@ const LogoHeader: React.FC<LogoHeaderProps> = ({
           <CustomSignIn />
         </SignedOut>
         <SignedIn>
+        <Link href="/dashboard" className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
+            Dashboard
+          </Link>
           <UserButton />
         </SignedIn>
       </div>
