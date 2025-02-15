46361d154d2607694250a422171271c560ba70db
unknown <huima.jose@hotmail.com>
2025-02-15 10:52:56 +0100

    Removed button in successmessage component

--- src/components/SuccessMessage.tsx
+++ src/components/SuccessMessage.tsx
@@ -10,12 +10,7 @@ const SuccessMessage = ({ message }: { message: string }) => {
     <div className="fixed top-4 right-4 bg-orange-100 text-orange-700 p-6 rounded-xl shadow-lg flex items-center space-x-3 w-[350px] text-lg">
       <CheckCircle size={32} className="text-orange-600" />
       <span className="flex-grow">{message}</span>
-      <button
-        onClick={() => setVisible(false)}
-        className="text-orange-800 text-sm font-semibold hover:underline"
-      >
-        Fechar
-      </button>
+      
     </div>
   );
 };
